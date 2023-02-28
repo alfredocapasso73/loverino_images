c7094443bfaa71bb5ed724b87990f1f30e16d5a5
Alfredo Capasso <alfredocapasso@AirsomtrAlfredo.lan>
2023-02-28 13:35:20 +0100

    cors added

--- server.js
+++ server.js
@@ -3,10 +3,25 @@ const cors = require('cors');
 const app = express();
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+
+const cors_origins = [];
+if(process.env.IS_LOCAL){
+    cors_origins.push('http://texas.localoverino.se:8080');
+    cors_origins.push('http://localoverino.se:8080');
+}
+else{
+    cors_origins.push('https://www.loverino.se');
+    cors_origins.push('https://loverino.se');
+    cors_origins.push('https://texas.loverino.se');
+}
+app.use(cors({
+    origin: cors_origins,
+    methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH']
+}));
+
+
 const auth = require('./middlewares/auth');
 const helper = require('./helpers/helper');
-
 
 app.get("/getImage/:image", async (req, res) => {
     return helper.getImage(req, res);
